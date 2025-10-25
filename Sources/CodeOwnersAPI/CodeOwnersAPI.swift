70e16580125bf5ee6a3e56041d014f5a3f0da048
Guillermo Mazzola <gmazzo65@gmail.com>
2025-10-25 10:49:23 +0200

    Added a `CodeOwners` typealias to API (#27)

--- Sources/CodeOwnersAPI/CodeOwnersAPI.swift
+++ Sources/CodeOwnersAPI/CodeOwnersAPI.swift
@@ -3,21 +3,23 @@ import Foundation
 nonisolated(unsafe) private var ownersCache: [Substring: CodeOwnersMappingProvider.Type] = [:]
 private let writeLock = NSLock()
 
+public typealias CodeOwners = Set<String>
+
 public protocol CodeOwnersMappingProvider  {
-    static var codeOwners: [Substring: Set<String>]? { get }
+    static var codeOwners: [Substring: CodeOwners]? { get }
 }
 
 private class Missing : CodeOwnersMappingProvider {
-    static let codeOwners: [Substring: Set<String>]? = nil
+    static let codeOwners: [Substring: CodeOwners]? = nil
 }
 
-public func codeOwnersOf(_ of: Any?) -> Set<String>? {
+public func codeOwnersOf(_ of: Any?) -> CodeOwners? {
     if of == nil { return nil }
     let type = type(of: of!)
     return codeOwnersOf(nsClassName: "\(String(reflecting: type))")
 }
 
-public func codeOwnersOf(nsClassName: String) -> Set<String>? {
+public func codeOwnersOf(nsClassName: String) -> CodeOwners? {
     let parts = nsClassName.split(separator: ".", maxSplits: 3)
     if parts.count < 2 { return nil }
     
@@ -29,7 +31,7 @@ public func codeOwnersOf(nsClassName: String) -> Set<String>? {
 #if os(Linux)
 @available(*, deprecated, message: "Call stacks are not fully supported on Linux")
 #endif
-public func codeOwnersOfCallStack(symbols: [String] = Thread.callStackSymbols, demangle: Bool = true) -> Set<String>? {
+public func codeOwnersOfCallStack(symbols: [String] = Thread.callStackSymbols, demangle: Bool = true) -> CodeOwners? {
     for symbol in symbols {
         guard let className = classNameFromSymbol(symbol, demangle) else { continue }
         if let owners = codeOwnersOf(nsClassName: "\(className)") { return owners }
@@ -37,7 +39,7 @@ public func codeOwnersOfCallStack(symbols: [String] = Thread.callStackSymbols, d
     return nil
 }
 
-private func resolve(_ moduleName: Substring) -> [Substring: Set<String>]? {
+private func resolve(_ moduleName: Substring) -> [Substring: CodeOwners]? {
     if let cached = ownersCache[moduleName] { return cached.codeOwners }
     
     let provider = (NSClassFromString("\(moduleName)._CodeOwners") as? CodeOwnersMappingProvider.Type) ?? Missing.self
