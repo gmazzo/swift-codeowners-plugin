f6c74c4d510fe33d5b977b4284d0dec19b50f015
Guillermo Mazzola <gmazzo65@gmail.com>
2025-10-26 20:38:15 +0100

    Moved `Settings` file logic to `Tool` (#31)

--- Plugins/CodeOwnersPlugin/CodeOwnersPlugin.swift
+++ Plugins/CodeOwnersPlugin/CodeOwnersPlugin.swift
@@ -18,30 +18,14 @@ struct CodeOwnersPlugin: BuildToolPlugin {
 
         let tool = try context.tool(named: "CodeOwnersTool")
         let outputFile = context.pluginWorkDirectoryURL.appendingPathComponent("CodeOwners.swift")
-        let extraArgs = try toolArgsFromSettings(context.package.directoryURL.appending(component: ".codeowners-tool.json"))
 
         return [.buildCommand(
             displayName: "CodeOwner attribution",
             executable: tool.url,
-            arguments: inputFiles.map(\.path) + ["--output-file", outputFile.path] + extraArgs,
+            arguments: inputFiles.map(\.path) + ["--output-file", outputFile.path],
             inputFiles: inputFiles,
             outputFiles: [outputFile]
         )]
-    }
-    
-    private func toolArgsFromSettings(_ settingsFile: URL) throws -> [String] {
-        if !FileManager.default.fileExists(atPath: settingsFile.path) { return [] }
-        
-        let data = try Data(contentsOf: settingsFile)
-        let settings = try JSONDecoder().decode(Settings.self, from: data)
-        
-        var args: [String] = []
-        if let root = settings.codeowners?.root { args += ["--codeOwnersRoot", root] }
-        if let file = settings.codeowners?.file { args += ["--codeOwnersFile", file] }
-        if let renames = settings.renames { args += renames.flatMap { (regEx, replacement) in ["--rename", "\(regEx)=\(replacement)"] } }
-        if let quiet = settings.quiet { args.append(quiet ? "--quiet" : "--no-quiet") }
-        if let verbose = settings.verbose { args.append(verbose ? "--verbose" : "--no-verbose") }
-        return args
     }
 
 }
--- Sources/CodeOwnersTool/RenameRule.swift
+++ Sources/CodeOwnersTool/RenameRule.swift
@@ -7,9 +7,7 @@ struct RenameRule: ExpressibleByArgument {
     
     init?(argument: String) {
         let parts = argument.split(separator: "=", maxSplits: 2)
-        guard parts.count == 2 || argument.last == "=" else {
-            fatalError("Rename rule should be in the <regex>=<replacement> format: \(argument)")
-        }
+        guard parts.count == 2 || argument.last == "=" else { return nil }
         self.regex = try! Regex("\(parts[0])")
         self.replacement = parts.count == 2 ? parts[1] : ""
     }
