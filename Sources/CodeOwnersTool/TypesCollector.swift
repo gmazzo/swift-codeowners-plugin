bc1c29ab0cbe5984b76bb8cd60e8a0879784c523
Guillermo Mazzola <gmazzo65@gmail.com>
2025-12-02 13:05:23 +0100

    Simplified `TypeCollector` code (#49)

--- Sources/CodeOwnersTool/TypesCollector.swift
+++ Sources/CodeOwnersTool/TypesCollector.swift
@@ -5,36 +5,56 @@ class TypesCollector: SyntaxVisitor {
     var extensionTypes: Set<Substring> = []
 
     override func visit(_ node: StructDeclSyntax) -> SyntaxVisitorContinueKind {
-        return visit(node.name.text, node.modifiers, &rootTypes)
+        visit(node, &rootTypes)
     }
 
     override func visit(_ node: ClassDeclSyntax) -> SyntaxVisitorContinueKind {
-        return visit(node.name.text, node.modifiers, &rootTypes)
+        visit(node, &rootTypes)
     }
 
     override func visit(_ node: EnumDeclSyntax) -> SyntaxVisitorContinueKind {
-        return visit(node.name.text, node.modifiers, &rootTypes)
+        visit(node, &rootTypes)
     }
 
     override func visit(_ node: FunctionDeclSyntax) -> SyntaxVisitorContinueKind {
-        return node.parent?.kind != .memberBlockItem ? visit(node.name.text, node.modifiers, &rootTypes) : .skipChildren
+        node.isTopLevel ? visit(node, &rootTypes) : .skipChildren
     }
 
     override func visit(_ node: ExtensionDeclSyntax) -> SyntaxVisitorContinueKind {
-        return visit(node.extendedType.trimmedDescription, node.modifiers, &extensionTypes)
+        visit(node.extendedType.trimmedDescription, node, &extensionTypes)
     }
     
     override func visit(_ node: ProtocolDeclSyntax) -> SyntaxVisitorContinueKind {
-        return .skipChildren // protocols by itself can't be instantiated
+        .skipChildren // protocols by itself can't be instantiated
     }
     
-    private func visit(_ name: String, _ modifiers: DeclModifierListSyntax, _ into: inout Set<Substring>) -> SyntaxVisitorContinueKind {
-        if !modifiers.contains(where: { $0.name.text == "private" }) {
+    private func visit<Node : WithModifiersSyntax>(_ node: Node, _ into: inout Set<Substring>) -> SyntaxVisitorContinueKind where Node : NamedDeclSyntax {
+        visit(node.name.text, node, &into)
+    }
+    
+    private func visit<Node : WithModifiersSyntax>(_ name: String, _ node: Node, _ into: inout Set<Substring>) -> SyntaxVisitorContinueKind {
+        if !node.isPrivate {
             let parts = name.split(separator: ".", maxSplits: 2) // we only care about the top level type
             
             into.insert(parts[0])
         }
         return .skipChildren
     }
+
+}
+
+private extension FunctionDeclSyntax {
+
+    var isTopLevel: Bool { get { parent?.kind != .memberBlockItem } }
+
+}
+
+private extension WithModifiersSyntax {
+
+    private func contains(_ modifier: String) -> Bool {
+        return modifiers.contains(where: { $0.name.text == modifier })
+    }
+
+    var isPrivate: Bool { get { contains("private" ) } }
 
 }
