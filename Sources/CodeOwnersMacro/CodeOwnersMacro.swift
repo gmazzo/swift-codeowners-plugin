b59ab4ccfb33595dccaddd614d0b670f175ec628
Guillermo Mazzola <gmazzo65@gmail.com>
2025-12-13 18:08:40 +0100

    Fixed path processing in macros

--- Sources/CodeOwnersMacro/CodeOwnersMacro.swift
+++ Sources/CodeOwnersMacro/CodeOwnersMacro.swift
@@ -14,13 +14,12 @@ extension CodeOwnersMacroBase {
         of node: some FreestandingMacroExpansionSyntax,
         in context: some MacroExpansionContext
     ) throws -> ExprSyntax {
-        guard let location = context.location(of: node, at: .beforeLeadingTrivia, filePathMode: .filePath) else {
+        guard let file = context.location(of: node, at: .afterLeadingTrivia, filePathMode: .filePath)?.file else {
             throw CodeOwnersMacroError(message: "Can't find current file location")
         }
-        let filePath = location.file.trimmedDescription
-        let file = try JSONDecoder().decode(String.self, from: filePath.data(using: .utf8)!)
         
-        let owners = resolver.codeOwnersOf(Path(file))
+        let filePath = Path(stringLiteral: file.description)
+        let owners = resolver.codeOwnersOf(filePath)
         return ExprSyntax(literal: owners)
     }
     
