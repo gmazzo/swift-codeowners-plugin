6c38b5d11ad5586d0f300ef2c7e5ad85990e287d
Guillermo Mazzola <gmazzo65@gmail.com>
2026-06-19 13:29:10 +0200

    Iterated `CodeOwnersMacroBase` ABI (#82)

--- Sources/CodeOwnersResolver/Path+RelativePath.swift
+++ Sources/CodeOwnersResolver/Path+RelativePath.swift
@@ -3,7 +3,7 @@ import PathKit
 public extension Path {
 
     // from https://stackoverflow.com/a/48360631/1007772
-    func relativePathTo(_ base: Path) -> String? {
+    func relativePathTo(_ base: Path) -> String {
         // Remove/replace "." and "..", make paths absolute:
         let destComponents = self.url.standardized.pathComponents
         let baseComponents = base.url.standardized.pathComponents
